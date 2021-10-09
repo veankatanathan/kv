46fc3828e579ba03eb7a3e13a243cc1bdabce9e1
veankatanathan <veankatanathan@gmail.com>
2021-10-09 23:59:36 +0530

    taskmaster

---
+++ src/org/selinium/Redbus.java
@@ -0,0 +1,13 @@
+package org.selinium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Redbus {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "F:\\Users\\admin\\eclipse-workspace\\Selenium\\Driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.redbus.in/");
+	
+}
+}
---
+++ src/org/selinium/TodayClass.java
@@ -0,0 +1,19 @@
+package org.selinium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TodayClass {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "F:\\Users\\admin\\eclipse-workspace\\Selenium\\Driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get(" http://www.flipkart.com/");
+	String url = driver.getCurrentUrl();
+	System.out.println(url);
+	String title = driver.getTitle();
+	System.out.println(title);
+	
+	}
+
+	
+}
